93740caf3643da45a22220eac4ba34042447d758
anirudh-shroff <shroffstar8@gmail.com>
2025-10-01 15:13:53 +0530

    some looping revision

---
+++ JS-7/assets/js/script.js
@@ -0,0 +1,65 @@
+// *loops*
+
+// 1. intialization
+// 2. condition
+// 3. modification
+
+// * while : it an entry control loop
+
+// let i = 1;
+// while(i <= 5){
+// console.log(`${i}"Hello"`);
+// i++;
+// }
+
+// console.log(i); here the output is 6 since it was trying add 1 to value of i since it's not 0, it is a true value
+
+// let i = 1;
+// while(i <= 5){
+// console.log(parseInt(Math.random() * 100));
+// i++;
+// }
+
+// console.log(i);
+
+// * do while : it is an exit control loop, the code will run at least one time as the condition is checked at the end
+
+// let i = 1;
+// do {
+//     console.log(parseInt(Math.random() * 100));
+//     i++;
+// } while (i <= 5);
+
+// * for
+
+// for(let i = 1; i <=5; i++){
+// console.log(parseInt(Math.random() * 100));
+// }
+
+// *forin loop:
+
+// for (const key in object) {
+//     if (!Object.hasOwn(object, key)) continue;
+    
+//     const element = object[key];
+    
+    
+// }
+
+// *forof loop:
+
+// for (const element of object) {
+    
+// }
+
+// *foreach lop:
+
+// array.forEach(element => {
+    
+// });
+
+// *map
+
+// *filter
+
+// *reduce
