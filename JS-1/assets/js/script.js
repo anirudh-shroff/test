75931a078d2e146f78da2eca5b5676547d3e1c7d
anirudh-shroff <shroffstar8@gmail.com>
2025-10-01 11:22:44 +0530

    folder update

---
+++ JS-1/assets/js/script.js
@@ -0,0 +1,44 @@
+// console.log(123);
+
+// window - entire browser area
+// document is entire white area where html is displayed (webpages)
+
+// window.alert("123");
+// it shows alert on the while loading the page is loading
+
+// document.write("This id writen using js");
+
+// prompt("do you want to continue");
+// prompt assk for user input before loading the page, conditions can be added to code to form a decision
+
+//what is a variable? - it is a container to store data and it can be accessed later.
+//three types of variables in js:
+
+// var - ES5
+// let - ES6
+// const - ES6
+
+// in js, any datatype can be added to a variable, datatype need not be declared in front of variable like done in c++
+
+// Datatypes:
+
+// let a = 10;          integer
+// let a = b;           string
+// let a = 3.14;        float
+// let a = null;
+// let a = Infinity;    
+// let a = undefined;
+// let a = true;        boolean value
+
+// and some miscelaneous datatypes* for later reference
+
+// let a = "hello";
+// console.log(a);
+
+// undefined - the default value of a  variable, which later changes when that variable is assigned a value
+
+// const is used when you don't want to value of variable to change
+// 1.   let cannot be reinitialized, it is blockscoped. not exposed in window object, so mostly used instead of var
+// 2.   const cannot be reinitialized, it is blockscoped as well, aslo it is a contant value, it is also  not exposed but has limited use cases.
+// 3.   var can be reinitialized. it is also exposed in window object through which sensitive data can be accessed. so it should be used in a function but not in global scope
+
