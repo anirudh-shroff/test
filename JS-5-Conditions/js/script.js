567f56dc90bbb2f9b82cce4e36db9f0d8bdc375a
anirudh-shroff <shroffstar8@gmail.com>
2025-10-01 11:26:10 +0530

    third push

---
+++ JS-5-Conditions/js/script.js
@@ -0,0 +1,32 @@
+// if(){};
+// if(){
+
+// }else(){
+
+// };
+// let a = 10;
+// if (a = 10) {
+//     console.log("yes");
+// } else {
+//     console.log("no");
+// };
+
+// let a = undefined; null are false values
+// anything else is a true value
+
+// if(a){
+//     console.log(a);
+// }else{
+//     console.log("no");
+// }
+// let b = 10;
+// if ("ABC" == "ABC") {
+//     console.log(b);
+// }
+// switch (b) {
+//     case 10:
+//         console.log(b);
+//     default:
+//         console.log("NO")
+// }
+// syntax is similar to c++ except a few things like declaring the variable and print
