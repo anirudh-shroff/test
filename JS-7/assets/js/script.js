775b8d2329f5d28503a9991074b3cbe4ac0c74a0
anirudh-shroff <shroffstar8@gmail.com>
2025-10-01 15:52:17 +0530

    forin and forof loops

--- JS-7/assets/js/script.js
+++ JS-7/assets/js/script.js
@@ -36,21 +36,47 @@
 // console.log(parseInt(Math.random() * 100));
 // }
 
-// *forin loop:
+// let sum = 0;
+// for(let i = 1; i <= 5; i++){
+// sum += i;
+// console.log(sum);
+// }
+
+// *forin loop: used for objects mostly.
+
 
 // for (const key in object) {
 //     if (!Object.hasOwn(object, key)) continue;
     
 //     const element = object[key];
-    
-    
+   
 // }
 
-// *forof loop:
+// const arr = [3, 5, 7];
+// arr.foo ="hello";
+
+// for (const i in arr) {
+//     console.log(i);
+// }
+
+// *forof loop: used for arrays, map, set, arguments mostly.
 
 // for (const element of object) {
-    
+// for (const i of arr) {
+//     console.log(i);
+// }    
 // }
+// as you can see above, forin iterates over property names, forof iterates over property values. They can also be used destructuring
+// [The destructuring syntax is a JavaScript syntax that makes it possible to unpack values from arrays, or properties from objects, into distinct variables. It can be used in locations that receive data (such as the left-hand side of an assignment or anywhere that creates new identifier bindings).]. 
+
+// let a, b, rest;
+// // [a, b] = [10, 20]
+// [a,b,...rest] = [10, 20, 30, 40, 50];
+// console.log(a);
+// console.log(b);
+// console.log(a,b,rest)
+
+// for example, you can simultaneously loop over the keys and values of an object using Object.entries().
 
 // *foreach lop:
 
